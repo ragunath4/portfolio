35ea6015af8cd4441aeaa887c90fd0174692979b
Ragunath P <124849401+ragunath4@users.noreply.github.com>
2025-07-21 17:01:42 +0530

    Update gettingString.java

--- gettingString.java
+++ gettingString.java
@@ -2,7 +2,7 @@ public java.util.scanner;
 public class gettingString{
 
     public static void main(String[]args)
-    {
-        System.out.println(Hello World Ragu karthi)
+    {i
+        System.out.println(Hello World Ragu k)
     }
 }
