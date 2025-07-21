b2f43ffba03b5a4bacb903403ceaa023fa8bed70
Ragunath P <124849401+ragunath4@users.noreply.github.com>
2025-07-21 17:25:06 +0530

    Update gettingString.java

--- gettingString.java
+++ gettingString.java
@@ -3,6 +3,6 @@ public class gettingString{
 
     public static void main(String[]args)
     {i
-        System.out.println(Hello World Ragu kaarthik)
+        System.out.println(Hello World Ragu kaarthik  uuu)
     }
 }
